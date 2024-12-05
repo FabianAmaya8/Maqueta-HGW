b360194434c750b66466d018d4c2ce87ace3eb4f
matcar12345 <matcar1229@gmail.com>
2024-12-05 09:24:21 -0500

    modified:   Admin/Administrador/Index.html 	modified:   Admin/Administrador/javaScript/javaS.js 	modified:   Admin/Administrador/styles/header.css 	new file:   Admin/Administrador/styles/iconos/logo.png 	new file:   Admin/Administrador/styles/iconos/usuario-gray.png 	new file:   Admin/Administrador/styles/iconos/usuario-relleno.png 	modified:   Admin/Administrador/styles/nav-bar.css 	modified:   Admin/Administrador/styles/section.css 	new file:   Admin/Administrador/styles/usuario.css

--- Admin/Administrador/javaScript/javaS.js
+++ Admin/Administrador/javaScript/javaS.js
@@ -13,3 +13,67 @@ function nav_bar(){
         section.className = "section";
     }
 }
+function usuario(){
+    let nav_usuario = document.getElementById("nav-usuario");
+    let separador = document.getElementById("separador");
+    let usuario = document.getElementById("usuario");
+    if(nav_usuario.className == "nav-usuario"){
+        nav_usuario.className = "nav-usuario on-usuario";
+        separador.className = "separador on-separador";
+        usuario.className = "usuario usuario-logo-on";
+    }
+    else if(nav_usuario.className == "nav-usuario on-usuario"){
+        nav_usuario.className = "nav-usuario";
+        separador.className = "separador";
+        usuario.className = "usuario";
+    }
+}
+window.onload = function(){
+    let canva1 = document.getElementById("canva1").getContext("2d");
+    canva1.height = 100;
+    canva1.width = 100;
+    let graficoBarras = new Chart(canva1, {
+        type: "bar",
+        data:{
+            labels: ["Enero", "Febrero", "Marzo"],
+            datasets: [{
+                label: ["Ganancias"],
+                data: [25, 45, 100],
+                backgroundColor: ["#8DC989", "rgb(255, 182, 193)", "rgb(255, 255, 120)"],
+            }]
+        },
+        options:{
+            responsive: true
+        }
+    });
+    let canva2 = document.getElementById("canva2").getContext("2d");
+    let graficoCirculo = new Chart(canva2, {
+        type: "pie",
+        data:{
+            labels: ["Alimentos", "Barras", "Bebidas"],
+            datasets:[{
+                label: "Porcentaje de compra",
+                data: [25, 20, 55],
+                backgroundColor: ["rgb(176, 234, 172)", "rgb(255, 182, 193)", "rgb(255, 255, 120)"],
+            }]
+        },  
+        options:{
+            responsive: true
+        }
+    });
+    let canva3 = document.getElementById("canva3").getContext("2d");
+    let graficoLineas = new Chart(canva3, {
+        type: "line",
+        data:{
+            labels: ["Clientes Registrados", "Visitas no registradas"],
+            datasets:[{
+                label: "Cantidad",
+                data: [32, 68],
+                backgroundColor: ["rgb(255, 182, 193)", "rgb(173, 216, 230)"],
+            }]
+        },
+        options:{
+            responsive: true
+        }
+    });
+}
