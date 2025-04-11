8e78e9bbb2bc5a3d215b76e77f4961d99923ac49
matcar12345 <matcar1229@gmail.com>
2025-04-11 08:51:57 -0500

    Productos funcional y logica Bonos
    Funcionalidad de productos

---
+++ Servidor/app/static/Js/Categorias/CrearCategorias.js
@@ -0,0 +1,24 @@
+import { crearFormulario } from '../Productos/crearProductos.js';
+import { alerta } from '../Productos/crearProductos.js';
+let datos_from_categoria = [{valor: "Nombre Categoria", tipo:"text", form:"input", required: "required"}];
+export function crearCategoria(){
+    crearFormulario([
+        {valor: "Nombre Categoria", tipo:"text", form:"input", required: "required"},
+    ], {valor: "Crear Categoria", url: "agregarCategoria()"}, [])
+}
+export function agregarCategoria(){
+    if (document.getElementById("formulario").checkValidity()) {
+        let objeto = {};
+        datos_from_categoria.forEach(valor => {
+            objeto[valor.valor] = (valor.valor != "Categoria" ? document.getElementById(valor.valor).value : parseInt(document.getElementById(valor.valor).value));
+        });
+        console.log(objeto);
+        fetch("http://127.0.0.1:5000/registro_categoria", {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(objeto)
+        }).then(state => state.text()).then(respuesta => {
+            alerta(respuesta);
+        });
+    }
+}
