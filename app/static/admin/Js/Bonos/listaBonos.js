e6a7197fc8f72b197950ad70d42aa53e9d5b1acb
pibe0710 <ksmz2006@gmail.com>
2025-04-21 06:32:02 +0200

    bonos conectado

--- app/static/admin/Js/Bonos/listaBonos.js
+++ app/static/admin/Js/Bonos/listaBonos.js
@@ -1,10 +1,17 @@
 import { contenido, datos_inicio, productos, categorias, categoriasVar } from '../variablesGlobales.js';
 import { crearLista } from '../Productos/listaProductos.js';
 export function listaBonos() {
-    contenido.innerHTML = `
-        ${crearLista(
-        [{ columna: "Id" }, { columna: "Nombre Bono" }, { columna: "Porcentaje" }, { columna: "Tipo" }, { columna: "Costo" }],
-        [{ fila1: ["hola", "como"] }, { fila2: ["dsa", "dsa"] }]
-    )}
-    `;
+        let tabla = "bonos";
+        let columna = "id_bonos";
+        let posicion_id = 0;
+        fetch("http://127.0.0.1:5000/Bonos").then(state => state.json()).then(valores => {
+            fetch("http://127.0.0.1:5000/encabezadoBonos").then(state => state.json()).then(encabezado => {
+                contenido.innerHTML = `
+                ${crearLista(
+                    encabezado
+                    , valores, tabla, columna, posicion_id
+                )}
+            `;
+            });
+        });
 }
--- app/static/admin/Js/main.js
+++ app/static/admin/Js/main.js
@@ -4,7 +4,7 @@ import { crearProductos, agregarProducto, alerta } from './Productos/crearProduc
 import { productosLista } from './Productos/listaProductos.js';
 import { editarProducto, editarProductoBoton, eliminarProducto } from './Productos/eliminarEditarProducto.js';
 import { listaBonos } from './Bonos/listaBonos.js';
-import { administrarBonos } from './Bonos/administrarBonos.js';
+import { administrarBonos, agregarBono } from './Bonos/administrarBonos.js';
 import { crearCategoria, agregarCategoria } from './Categorias/CrearCategorias.js';
 import { listaCategorias } from './Categorias/ListaCategorias.js'
 import { crearSubcategoria, agregarSubcategoria } from './Categorias/subcategorias/CrearSubcategoria.js';
@@ -31,3 +31,4 @@ window.editarProductoBoton = editarProductoBoton;
 window.eliminarProducto = eliminarProducto;
 window.listaBonos = listaBonos;
 window.administrarBonos = administrarBonos;
+window.agregarBono = agregarBono;
